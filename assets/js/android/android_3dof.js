6e66cc3ce7b5428d73a70d283940fa70a9cbead1
monkeyMax-SVG <monkeymax@yeah.net>
2021-03-13 07:54:33 +0800

    merge scene and video frame

--- assets/js/android/android_3dof.js
+++ assets/js/android/android_3dof.js
@@ -146,26 +146,106 @@ const resetImagePosition = () => {
   }
 }
 
+function resizeCanvas(origCanvas, width, height)
+{
+    let resizedCanvas = document.createElement("canvas");
+    let resizedContext = resizedCanvas.getContext("2d");
+
+    if (screen.width < screen.height)
+    {
+        var w = height * (height / width);
+        var h = width * (height / width);
+        var offsetX = -(height - width);
+    }
+    else
+    {
+        var w = width;
+        var h = height;
+        var offsetX = 0;
+    }
+    resizedCanvas.height = height;
+    resizedCanvas.width = width;
+
+    resizedContext.drawImage(origCanvas, offsetX, 0, w, h);
+    return resizedCanvas.toDataURL();
+}
+
+function captureVideoFrame(video, format, width, height)
+{
+    if (typeof video === 'string')
+    {
+        video = document.querySelector(video);
+    }
+
+    format = format || 'jpeg';
+
+    if (!video || (format !== 'png' && format !== 'jpeg'))
+    {
+        return false;
+    }
+
+    var canvas = document.createElement("CANVAS");
+
+    canvas.width = width || video.videoWidth;
+    canvas.height = height || video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    var dataUri = canvas.toDataURL('image/' + format);
+    var data = dataUri.split(',')[1];
+    var mimeType = dataUri.split(';')[0].slice(5)
+
+    var bytes = window.atob(data);
+    var buf = new ArrayBuffer(bytes.length);
+    var arr = new Uint8Array(buf);
+
+    for (var i = 0; i < bytes.length; i++)
+    {
+        arr[i] = bytes.charCodeAt(i);
+    }
+
+    var blob = new Blob([ arr ], { type: mimeType });
+    return { blob: blob, dataUri: dataUri, format: format, width: canvas.width, height: canvas.height };
+}
+
 const takePicture = () => {
-  const screenshotTarget = document.querySelector("a-scene");
+  let video = document.querySelector("video");
+  video.pause();
 
-  html2canvas(screenshotTarget).then((canvas) => {
-      const base64image = canvas.toDataURL("image/png");
-      // window.location.href = base64image;
+  let aScene = document.querySelector("a-scene").components.screenshot.getCanvas("perspective");
+  
+  let frame = captureVideoFrame("video", "png");
+  
+  aScene = resizeCanvas(aScene, frame.width, frame.height);
+  frame = frame.dataUri;
 
-      var fileName = 'webar-experience' + '-' + Date.now() + '.png';
-      var linkEl = document.createElement('a');
-      var url = base64image;
-      linkEl.href = url;
-      linkEl.setAttribute('download', fileName);
-      linkEl.innerHTML = 'downloading...';
-      linkEl.style.display = 'none';
-      document.body.appendChild(linkEl);
-      setTimeout(function () {
-        linkEl.click();
-        document.body.removeChild(linkEl);
-      }, 1);
+  mergeImages([frame, aScene]).then(b64 =>
+  {
+    let link = document.getElementById("download-link", "png");
+
+    link.setAttribute("download", "AR.png");
+    link.setAttribute("href", b64);
+    link.click();
   });
+
+  video.play();
+  // const screenshotTarget = document.querySelector("a-scene");
+
+  // html2canvas(screenshotTarget).then((canvas) => {
+  //     const base64image = canvas.toDataURL("image/png");
+  //     // window.location.href = base64image;
+
+  //     var fileName = 'webar-experience' + '-' + Date.now() + '.png';
+  //     var linkEl = document.createElement('a');
+  //     var url = base64image;
+  //     linkEl.href = url;
+  //     linkEl.setAttribute('download', fileName);
+  //     linkEl.innerHTML = 'downloading...';
+  //     linkEl.style.display = 'none';
+  //     document.body.appendChild(linkEl);
+  //     setTimeout(function () {
+  //       linkEl.click();
+  //       document.body.removeChild(linkEl);
+  //     }, 1);
+  // });
     // document.querySelector('a-scene').components.screenshot.capture('perspective');
 }
 
