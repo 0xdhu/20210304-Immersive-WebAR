f5786e54dd593d80e5d70a446c4312455f70767a
monkeyMax-SVG <monkeymax@yeah.net>
2021-03-18 21:58:22 +0800

    xx

--- assets/js/android/android_3dof.js
+++ assets/js/android/android_3dof.js
@@ -345,8 +345,8 @@ const takeRecord = () => {
     let video = document.querySelector("video");
     // video.pause();
 
-    let aScene = document.querySelector("a-scene").components.screenshot.getCanvas("perspective");
-    // let aScene = document.querySelector("canvas[class='a-canvas a-grab-cursor']");
+    //let aScene = document.querySelector("a-scene").components.screenshot.getCanvas("perspective");
+    let aScene = document.querySelector("canvas[class='a-canvas a-grab-cursor']");
     console.log(aScene);
 
     let ww = aScene.width / aScene.height * screen.height;
