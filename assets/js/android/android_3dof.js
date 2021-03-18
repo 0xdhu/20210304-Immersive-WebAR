ff63333e65d195f06cfff1c8a85fe76641ab964b
monkeyMax-SVG <monkeymax@yeah.net>
2021-03-18 12:08:35 +0800

    xx

--- assets/js/android/android_3dof.js
+++ assets/js/android/android_3dof.js
@@ -377,6 +377,8 @@ const takeRecord = () => {
     // videoContext.drawImage(aScene, 0, 0, video.videoWidth, video.videoHeight);
 
     recordedVideo.add(videoContext);
+    recordedVideo.add(videoContext);
+    recordedVideo.add(videoContext);
     console.log("******** " + ctx);
     ctx++;
   } else {
